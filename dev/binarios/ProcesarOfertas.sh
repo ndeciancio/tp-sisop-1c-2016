7e95668fe32723bb9b72e05e93509023cf02de7b
Lautaro Rinaldi <lautaroezequielrinaldi@yahoo.com.ar>
2016-05-03 16:25:12 -0300

    agrego ProcesarOfertas.sh

---
+++ dev/binarios/ProcesarOfertas.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# Listo archivos que estan en OKDIR/
+
+IFS='
+'
+# Del padron, solo me interesa quedarme con el grupo y el número de orden y concatenarlos para formar el contrato_fusionado
+contratos_fusionados=$(cut -c1-4,6-8 MAEDIR/temaL_padron.csv.xls)
+
+# De grupos.csv, me interesan los que están en estado ABIERTO o NUEVO (es decir, todos los que son NO CERRADOS, -v invierte comportamiento)
+#nuevos_o_abiertos=$(grep -v "^[0-9]\{1,4\};CERRADO;[^;]*;[^;]*;[^;]*;[^;]*" MAEDIR/Grupos.csv)
+nuevos_o_abiertos=$(grep -v "^[0-9]\{1,4\};CERRADO;[^;]*;[^;]*;[^;]*;[^;]*" MAEDIR/Grupos.csv)
+#echo $nuevos_o_abiertos
+
+#nuevos_o_abiertos=$(grep "^[0-9]\{1,4\};ABIERTO;[^;]*;[^;]*;[^;]*;[^;]*" MAEDIR/Grupos.csv)
+
+echo "---------------------------------------"
+
+for archivo in $(ls OKDIR)
+do
+	echo $archivo
+	# Verificar que el archivo no este duplicado contra PROCDIR/procesados
+	exito=$(find "PROCDIR/procesadas/" -type f -name "$archivo" | wc -l)
+	if [ $exito -ne 0 ]
+	then
+		echo -e "\e[1;31m     Se rechaza el archivo por estar duplicado \e[0m"
+	else
+		# Verificar la cantidad de campos del primer registro
+	        read linea_archivo < OKDIR/$archivo
+	        echo $linea_archivo | grep -q "^[0-9]\{7\};[0-9].*$"
+	        # Verifico si la linea tiene el formato esperado. Si tuvo exito el grep (0) esta bien; sino fallo (1)
+	        exito=$?
+	        if [ $exito -ne 0 ]
+	        then
+			echo -e "\e[1;31m     Se rechaza el archivo porque su estructura no se corresponde con el formato esperado \e[0m"
+			# -------------------------------------
+			# Pendiente: Mover archivo a rechazados
+			# -------------------------------------
+	        else
+               		echo -e "\e[1;34m     Archivo a procesar: $archivo \e[0m"
+			# Verificar Ofertas Validas
+			while read -r linea_archivo
+			do
+				contrato_fusionado=$(echo $linea_archivo | cut -c1-7)
+				echo $contrato_fusionado
+				IFS=' '
+				echo $contratos_fusionados | grep -q "^$contrato_fusionado$" 
+				resultado=$?
+				if [ $resultado -eq 0 ]
+				then
+					# contrato_fusionado valido. Hay que validar que esté en abierto o nuevo
+					grupo=$(echo $linea_archivo | cut -c1-4)
+					orden=$(echo $linea_archivo | cut -c5-7)
+					importe_ofrecido=$(echo $linea_archivo | sed "s/^[^;]*;\([^;]*\)$/\1/")
+
+					# Reemplazo coma decimal por PUNTO decimal
+					importe_ofrecido=$(echo $importe_ofrecido | sed 's/\,/\./')
+					# Borro el retorno de carro que está al final de cada línea 
+					importe_ofrecido=$(echo $importe_ofrecido | sed 's/\r//')
+					echo "     ofrecio: $importe_ofrecido"
+
+					linea_val=$(echo $nuevos_o_abiertos | grep "^$grupo;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*$")
+					hay_resultados=$?
+					if [ $hay_resultados -ne 0 ]
+					then
+						echo -e "\e[1;34m Rechazado. Contrato no encontrado. \e[0m"
+					else
+						# Supero validación de Contrato Encontrado
+						echo "     $linea_val"
+	
+						cuota_pura=$(echo $linea_val | sed "s/^[^;]*;[^;]*;[^;]*;\([^;]*\);[^;]*;[^;]*$/\1/")
+						cuotas_pendientes=$(echo $linea_val | sed "s/^[^;]*;[^;]*;[^;]*;[^;]*;\([^;]*\);[^;]*$/\1/")
+						cuotas_para_licitar=$(echo $linea_val | sed "s/^[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;\([^;]*\)$/\1/")
+	
+						#Reemplazo la coma por el punto en el monto
+						valor_cuota=$(echo $cuota_pura | sed 's/\,/\./')
+	
+						valor_minimo_licitacion=$(echo $valor_cuota \* $cuotas_para_licitar | bc)
+						valor_maximo_licitacion=$(echo $valor_cuota \* $cuotas_pendientes | bc)
+	
+						echo "     minimo: $valor_minimo_licitacion"
+						echo "     maximo: $valor_maximo_licitacion"
+	
+						# ------------------------------------------	
+						# Aca se debe validar el monto de la oferta
+						# ------------------------------------------
+	
+						monto_debajo=$(echo "$importe_ofrecido < $valor_minimo_licitacion" | bc)
+						if [ $monto_debajo -eq 1 ]
+						then
+							echo -e "\e[1;34m Rechazado. No alcanza el monto mínimo.  \e[0m"
+						else
+							monto_encima=$(echo "$importe_ofrecido > $valor_maximo_licitacion" | bc)
+							if [ $monto_encima -eq 1 ]
+							then
+								echo -e "\e[1;34m Rechazado. Supera el monto máximo.  \e[0m"
+							else
+								# Supero validaciones de monto
+								
+								# -------------------------------------	
+								# Aca se debe validar FLAG si participa
+								# -------------------------------------
+
+# temaL_padron.csv 
+# grupo | orden	| nombre suscriptor | concesionario | coeficiente | participa | motivo | cuotas de recupero | cuotas de deuda | fecha 1º venc | 1º cuota con deuda |   deuda    | id susc 
+#   4   |   3   |        N          |        4      |   num       |     1     |   2    |     000000         |        00       |     00000000  |         00         | 0000000000 |    num
+
+linea_padron=$(grep "^$grupo;$orden;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*;[^;]*$" MAEDIR/temaL_padron.csv.xls)
+echo "     $linea_padron"
+flag_participa=$(echo $linea_padron | cut -f6 -d';')
+nombre_suscriptor=$(echo $linea_padron | cut -f3 -d';')
+usuario=$(whoami)
+concesionario=$(echo $archivo | sed "s/^\([^;]*\)_[^;]*$/\1/")
+fecha_archivo=$(echo $archivo | sed "s/^[^;]*_\([^;]*\)\.[^;]*\.[^;]*$/\1/")
+fecha_actual=$(date +"%Y%m%d_%T")
+echo "flag: $flag_participa"
+
+if [ $flag_participa -eq 1 ] || [ $flag_participa -eq 2 ]
+then
+	# Supero validación de flag de participacion
+	echo "ok. continuamos."
+	echo "$concesionario;$fecha_archivo;$contrato_fusionado;$grupo;$orden;$importe_ofrecido;$nombre_suscriptor;$usuario;$fecha_actual" >> salida.txt
+else
+	echo -e "\e[1;34m Rechazado. Suscriptor no puede participar.  \e[0m"
+fi
+							fi
+						fi
+					fi
+				fi
+			done < OKDIR/$archivo
+	        fi
+	fi
+	echo "---------------------------------------"
+done
+
